b714ed4dd21f3717d07840ead9f09d14b3c3ca74
gheun <gheun098@gmail.com>
2024-10-02 10:14:49 +0900

    1002 - 이진 검색 예제2 ( cnt : 비교횟수 추가)

--- ch04/binarysearch.c
+++ ch04/binarysearch.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+int cnt = 0;
 int binarySearch(int a[], int n, int key) {
 	int left = 0, mid, right = n - 1;
 
 	while (left <= right) {
 		mid = (left + right) / 2;
+		cnt++;
 		if (key == a[mid]) return mid;;
 		if (key > a[mid]) left = mid + 1;
 		else right=mid-1;
@@ -13,7 +15,7 @@ int binarySearch(int a[], int n, int key) {
 }
 
 int main(void) {
-	int i, index, key, n;
+	int index, key, n;
 	int a[] = { 3, 7, 10, 12, 14, 18, 21, 33, 35, 48 };
 	n = sizeof(a) / sizeof(int);
 
@@ -21,7 +23,7 @@ int main(void) {
 	scanf_s("%d", &key);
 	index = binarySearch(a, n, key);
 
-	if (index >= 0) printf("%d를(을) %d위치에서 찾음!\n", key, index);
+	if (index >= 0) printf("%d를(을) %d위치에서 %d회 비교하여 찾음!\n", key, index, cnt);
 	else printf("%d은(는) 없는 데이터임!\n");												//말투 되게 다긍이 같다
 	return 0;
 }
